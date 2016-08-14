52b9da1352b28e585c579ff112c7c56b4fc842f4
Juan P. Anaya <janayamuedano@fullsail.edu>
2016-08-14 14:45:13 -0400

    Lab CE05
    Media player fixed bugs on previous song clicked.
    Next step is to get the notification to work with the next and previous
    buttons using Broadcast Receivers and then make the application work in
    the foreground without the need of it being open.

--- AnayaJuan_CE05/app/src/main/java/com/paix/jpam/anayajuan_ce05/MediaActivity.java
+++ AnayaJuan_CE05/app/src/main/java/com/paix/jpam/anayajuan_ce05/MediaActivity.java
@@ -4,13 +4,36 @@
 
 package com.paix.jpam.anayajuan_ce05;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MediaActivity extends AppCompatActivity {
+import com.paix.jpam.anayajuan_ce05.services.AudioService;
+
+/*Implement the ServiceConnection Interface in the activity that is doing the binding, in this case
+Main Activity is doing the binding with the AudioService. The ServiceConnection interface contains
+ two callbacks that are used to relay information about the connection with the bound service.
+ onServiceConnected() & onServiceDisconnected() */
+public class MediaActivity extends AppCompatActivity implements ServiceConnection, OnMediaButtonClicked {
 
     //TAG
     private static final String TAG = "MediaActivity";
+    //Bound Flag
+    boolean mBound;
+    //Media Player Service
+    AudioService mService;
+    //SeekBar Flags (Fragment -> Activity Interface Flags)
+    boolean seekBarWillChange;
+    boolean seekBarStoppedChanges;
+    int newSeekBarValue;
 
     /*LifeCycle*/
     @Override
@@ -18,10 +41,179 @@ public class MediaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
 
-        //Set Fragment
+        Intent intent = new Intent(this, AudioService.class);
+        bindService(intent, this, BIND_AUTO_CREATE);
+
+        //Set Media Player Fragment
+        MediaFragment mPlayerFragment = new MediaFragment();
+        setMediaPlayerFragment(mPlayerFragment);
+
+        //Initialize SeekBar Flags & Value
+        seekBarWillChange = false;
+        seekBarStoppedChanges = false;
+        newSeekBarValue = 0;
+
+        //Register Local Broadcast Receivers -> Next Song
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mNextSongReceiver,
+                        new IntentFilter(AudioService.SONG_HAS_FINISHED_PLAY_NEXT));
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+         /*Set the Bound Flag to False when Activity Resumes, this stops the playback when we leave
+        * the activity*/
+        //mBound = false;
+        /*Bind to the service using the bindService() method, and pass in the same intent that we
+        * would use to start a service if it doesn't exist and don't let id die until we unbind.
+        * Binding to a service happens asynchronously so don't treat your service as bound
+         * immediately after calling bindService(). You will need to wait until the
+         * onServiceConnected() callback and then cast the iBinder Object to your Binder type and
+         * call any methods you wish to call on the Binder or the returned service. */
+        //Intent intent = new Intent(this, AudioService.class);
+        //bindService(intent, this, BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        /*It's important to note that we should not attempt to unbind from a service if we're not
+        * already bound, as attempting to do so will throw an IllegalArgumentException. Also since
+        * bound services can't stop while bound , do not try calling stopService() while the service
+        * is bound, as nothing will happen and it will be hard to debug.*/
+        //mBound = false;
+        //unbindService(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //Unregister Local Broadcast Receivers -> Next Song
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mNextSongReceiver);
+        //Unbind Service
+        mBound = false;
+        unbindService(this);
+    }
+
+    /*Custom Methods*/
+    //Set Media Player Fragment Method
+    private void setMediaPlayerFragment(MediaFragment mPlayerfragment) {
+        getSupportFragmentManager().beginTransaction().replace
+                (R.id.FrameLayout_MediaActivity, mPlayerfragment).commit();
+    }
+
+    /*SERVICE*/
+    @Override
+    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+        AudioService.AudioServiceBinder binder =
+                (AudioService.AudioServiceBinder) iBinder;
+        /*Once the Service is Connected , get the Service from the  casted iBinder Object*/
+        mService = binder.getService();
+        /*Service bound, it's time to update the Boolean Flag to let the Main Activity it can
+        * use the Service methods. */
+        mBound = true;
+        //Dev
+        Log.i(TAG, "onServiceConnected: " + "SERVICE CONNECTED");
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
+        //Dev
+        Log.i(TAG, "onServiceDisconnected: " + "SERVICE DISCONNECTED");
+    }
+
+    /*OnMediaButtonClicked INTERFACE*/
+    //Loop
+    @Override
+    public void onLoopClicked() {
+        mService.loop();
+    }
+
+    //Next
+    @Override
+    public void onNextClicked() {
+        //Next
+        mService.next();
+        //Play
+        mService.play();
+    }
+
+    //Pause
+    @Override
+    public void onPauseClicked() {
+        mService.pause();
+    }
+
+    //Play
+    @Override
+    public void onPlayClicked() {
+        mService.play();
+    }
+
+    //Previous
+    @Override
+    public void onPreviousClicked() {
+        //Previous
+        mService.previous();
+        //Play
+        mService.play();
+    }
+
+    //Shuffle
+    @Override
+    public void onShuffleClicked() {
+        mService.shuffle();
+    }
+
+    //Stop
+    @Override
+    public void onStopClicked() {
+        mService.stop();
+    }
+
+    /*SEEK_BAR*/
+    //Tracking Changes
+    @Override
+    public void onSeekBarStartedTrackingChanges() {
+        //SeekBarFlags
+        seekBarStoppedChanges = false;
+        seekBarWillChange = true;
+    }
+
+    //Stop Tracking Changes
+    @Override
+    public void onSeekBarStoppedTrackingChanges() {
+        //SeekBarFlags
+        seekBarWillChange = false;
+        seekBarStoppedChanges = true;
+    }
+
+    //Value Changed
+    @Override
+    public void onSeekBarValueChanged(int value) {
+        //Get New SeekBar Value
+        newSeekBarValue = value;
+        //Change Song Value
+        if (seekBarWillChange && mService.mPlayer.isPlaying()) {
+            mService.mPlayer.seekTo(newSeekBarValue * 1000);
+            Log.i(TAG, "onSeekBarValueChanged: " + "SONG SECOND: " + mService.mPlayer.getCurrentPosition());
+        }
+        //Dev
+        Log.i(TAG, "onSeekBarValueChanged: " + newSeekBarValue);
+    }
+
+    /*Broadcasts*/
+    //Broadcast Receiver on Song Completion & Not Looping -> Should Play Next Song
+    /*Broadcast Receivers*/
+    private BroadcastReceiver mNextSongReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Play Next Song
+            mService.play();
+        }
+    };
 
 
 }
--- AnayaJuan_CE05/app/src/main/java/com/paix/jpam/anayajuan_ce05/services/AudioService.java
+++ AnayaJuan_CE05/app/src/main/java/com/paix/jpam/anayajuan_ce05/services/AudioService.java
@@ -1,18 +1,35 @@
 package com.paix.jpam.anayajuan_ce05.services;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 
+import com.paix.jpam.anayajuan_ce05.R;
 import com.paix.jpam.anayajuan_ce05.Song;
+
+import java.util.Random;
 
 public class AudioService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
 
     //TAG
     private static final String TAG = "AudioService";
     /*States for Media Player Lifecycle: State Diagram: https://goo.gl/ii8xIP */
+//    @Retention(RetentionPolicy.class)
+//    @IntDef({STATE_IDLE,STATE_INITIALIZED,STATE_PREPARING,STATE_PREPARED,
+//            STATE_STARTED,STATE_PAUSED,STATE_STOPPED,STATE_PLAYBACK_COMPLETED,
+//            STATE_END})
+//    private interface PlayerState{}
     private static final int STATE_IDLE = 0;
     private static final int STATE_INITIALIZED = 1;
     private static final int STATE_PREPARING = 2;
@@ -43,7 +60,7 @@ public class AudioService extends Service implements MediaPlayer.OnPreparedListe
     private static final String SONG_PREVIOUS = "com.paix.jpam.j_anaya_ce05_mSongPrevious";
 
     //Media Player
-    MediaPlayer mPlayer;
+    public MediaPlayer mPlayer;
 
     //Song ArrayList
     Song[] songs;
@@ -52,19 +69,301 @@ public class AudioService extends Service implements MediaPlayer.OnPreparedListe
     //Shuffle Flag
     boolean isShuffling;
 
+    /*To implement a Binder, the First thing we need to do is create a public inner class in our
+    * service that extends Binder, then we can add whatever methods we want to that binder to
+    * access the service. The easiest way to do this is to create a single method in your binder
+    * class that returns an instance of the containing service. Source: https://goo.gl/AHDoVM */
+    public class AudioServiceBinder extends Binder {
+        public AudioService getService() {
+            return AudioService.this;
+        }
+    }
+
+    /*Life Cycle*/
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Notification to start service on foreground
+
+
+        //Populate Array with songs
+        songs = new Song[]{new Song("Gessaffelstain", "Aleph", "Aleph", R.drawable.gessaffelstein_aleph, R.raw.aleph),
+                new Song("Kimbra", "Settle Down", "Settle Down", R.drawable.kimbra_settle_down, R.raw.settle_down),
+                new Song("Soja", "Amid The Noise And Haste", "Your Song", R.drawable.soja, R.raw.your_song),
+                new Song("Broke Fro Free", "Something EP", "Something Elated", R.drawable.something_elated, R.raw.something_elated)};
+        songIndex = 0;
+
+        Log.i(TAG, "onCreate: " + songs.length);
+
+        mPlayer = new MediaPlayer();
+        mState = STATE_IDLE;
+        mPlayer.setOnPreparedListener(this);
+        mPlayer.setOnCompletionListener(this);
+        //Set Looping
+        //mPlayer.setLooping(false);
+        //Duration
+        mSongDuration = 0;
+        //Shuffle Flag
+        isShuffling = false;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //If Media Player exists, release.
+        if (mPlayer != null) {
+            mPlayer.release();
+            mState = STATE_END;
+            mPlayer = null;
+        }
+    }
+
+    /*PLAY*/
+    public void play() {
+        //If it already started skip all
+        if (mState != STATE_STARTED) {
+
+            //If it's paused resume
+            if (mState == STATE_PAUSED) {
+                mPlayer.start();
+                mState = STATE_STARTED;
+            } else {
+                //Reset Happened - Top of the Process
+                if (mState == STATE_IDLE) {
+                    try {
+                        //setDataSource
+//                        mPlayer.setDataSource(this, Uri.parse("android.resource://" +
+//                                getPackageName() + "/" + R.raw.something_elated));
+                        mPlayer.setDataSource(this, Uri.parse("android.resource://" +
+                                getPackageName() + "/" + songs[songIndex].getSong()));
+                        mState = STATE_INITIALIZED;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                //To State Preparing
+                if (mState == STATE_INITIALIZED || mState == STATE_STOPPED || mState == STATE_PLAYBACK_COMPLETED) {
+                    mPlayer.prepareAsync();
+                    mState = STATE_PREPARING;
+
+                    //When Song is preparing send Local Broadcast with Song Information for UI
+                    Intent intent = new Intent(SONG_INFORMATION);
+                    //Current Song
+                    Song currentSong = songs[songIndex];
+                    intent.putExtra("songInfo_key", currentSong);
+                    LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+
+                    //Start Notification
+                    foregroundNotification(currentSong.getArtwork(), currentSong.getArtist(), currentSong.getSongName());
+
+                }
+            }
+        }
+    }
+
+    /*PAUSE*/
+    public void pause() {
+        if (mState == STATE_STARTED) {
+            mPlayer.pause();
+            mState = STATE_PAUSED;
+        }
+    }//Finished
+
+    /*STOP*/
+    public void stop() {
+        if (mState == STATE_STARTED || mState == STATE_PAUSED ||
+                mState == STATE_PREPARED || mState == STATE_PLAYBACK_COMPLETED) {
+            mPlayer.stop();
+            mState = STATE_STOPPED;
+            //Stop Song Current Position Runnable
+            mHandler.removeCallbacks(songCurrentPositionUpdate);
+        }
+    }
+
+    /*Loop*/
+    public void loop() {
+        if (!mPlayer.isLooping()) {
+            mPlayer.setLooping(true);
+            //Dev
+            Log.i(TAG, "loop: " + mPlayer.isLooping());
+            return;
+        }
+        mPlayer.setLooping(false);
+        //Dev
+        Log.i(TAG, "loop: " + mPlayer.isLooping());
+    }
+
+    /*Next*/
+    public void next() {
+        //Stop Current Song Playback
+        mPlayer.stop();
+        mState = STATE_STOPPED;
+        if (!mPlayer.isLooping()) {
+            //The Media Player needs to read the next song -> change song index
+            if (!isShuffling) {
+                if (songIndex == songs.length - 1) { // Go back to first song
+                    songIndex = 0;
+                } else {
+                    songIndex++; // Next Available Song
+                }
+            } else {
+                //If it's set to shuffle, get a new Random Song Index
+                randomSongIndex();
+            }
+            //Reset Media Player
+            mPlayer.reset();
+            mState = STATE_IDLE;
+            return;
+        }
+        mPlayer.prepareAsync();//Note: Start from beginning to avoid errors
+        mState = STATE_PREPARING;
+
+    }
+
+    /*Previous*/
+    public void previous() {
+        //Stop Current Song Playback
+        mPlayer.stop();
+        mState = STATE_STOPPED;
+
+        if (!mPlayer.isLooping()) {
+            //The Media Player needs to read the previous song -> change song index
+            if (!isShuffling) {
+                if (songIndex == 0) {
+                    songIndex = (songs.length - 1);
+                } else {
+                    songIndex--;
+                }
+            } else {
+                randomSongIndex();
+            }
+            //Reset Media player
+            mPlayer.reset();
+            mState = STATE_IDLE;
+            return;
+        }
+
+        mPlayer.prepareAsync();//Note: Start from beginning to avoid errors
+        mState = STATE_PREPARING;
+    }
+
+    /*Shuffle*/
+    public void shuffle() {
+        if (!isShuffling) {
+            isShuffling = true;
+            return;
+        }
+        isShuffling = false;
+    }
+
+    /*Media Player On Prepared Listener - called with prepareAsync()*/
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        /*It's also ok to call .prepare() instead of prepareAsync() with files */
+        mState = STATE_PREPARED; //prepareAsync() finished
+        //Duration
+        mSongDuration = mPlayer.getDuration();//  When track is prepared
+        //Start Service
+        mPlayer.start();
+        mState = STATE_STARTED;
+        songCurrentPositionUpdate.run();
+    }
+
+    /*Get Values to Update SeekBar*/
+    /*A Handler allows you to send and process Message and Runnable objects associated with a
+    thread's MessageQueue. Each Handler instance is associated with a single thread and that
+    thread's message queue. When you create a new Handler, it is bound to the thread / message
+    queue of the thread that is creating it -- from that point on, it will deliver messages and
+    runnables to that message queue and execute them as they come out of the message queue.*/
+    private Runnable songCurrentPositionUpdate = new Runnable() {
+        @Override
+        public void run() {
+            if (mState == STATE_STARTED) {
+                int mProgress = mPlayer.getCurrentPosition() / 1000; //Seconds
+                //Broadcast Song Progress Update every second
+                Intent intent = new Intent(SONG_CURRENT_POSITION_UPDATE);
+                intent.putExtra("mProgress_key", mProgress);
+                intent.putExtra("mSongDuration_key", mSongDuration);
+                LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+            }
+            mHandler.postDelayed(this, 250);//Send Broadcast every 250 ms
+        }
+    };
+
+    /* On Media Player Completion Listener*/
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        mState = STATE_PLAYBACK_COMPLETED;
+        mPlayer.stop();
+        mState = STATE_STOPPED;
+        //IF The Loop Song button is Pressed
+        if (mPlayer.isLooping()) {
+            mPlayer.prepareAsync();//Note: Start from beginning to avoid errors
+            mState = STATE_PREPARING;
+            return;
+        }
+        //If Song has stopped and is not looping
+        if (mState == STATE_STOPPED) {
+            if (!isShuffling) {
+                //If the Media Player needs to read the next song change song index
+                if (songIndex == songs.length - 1) { // Go back to first song
+                    songIndex = 0;
+                } else {
+                    songIndex++; // Next Available Song
+                }
+            } else {
+                //If it's set to shuffle, get a new Random Song Index
+                randomSongIndex();
+            }
+            //Reset Media Player
+            mPlayer.reset();
+            mState = STATE_IDLE;
+            //Broadcast that the song has Finished to Activity
+            Intent intent = new Intent(SONG_HAS_FINISHED_PLAY_NEXT);
+            LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+        }
+    }
+
+
+    /*Return an Instance of the Binder in onBind. This allows all binding activities to directly
+       * access the service using a service object. IMPORTANT: You should never bind to a service
+       * if the onBind method returns null.*/
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new AudioServiceBinder();
     }
 
-    @Override
-    public void onCompletion(MediaPlayer mediaPlayer) {
-
+    /*Random Song Index*/
+    private void randomSongIndex() {
+        Random randomNumber = new Random();
+        songIndex = randomNumber.nextInt(songs.length);
+        //TODO random number generation should have logic if the Audio is streamed
     }
 
-    @Override
-    public void onPrepared(MediaPlayer mediaPlayer) {
+    //Foreground Notification
+    public void foregroundNotification(int icon, String artist, String songTitle) {
 
+        //Notification Builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(android.R.drawable.ic_media_play);
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), icon);
+        builder.setLargeIcon(largeIcon);
+        builder.setContentTitle(artist);
+        builder.setContentText(songTitle);
+        builder.setAutoCancel(false);
+        builder.setOngoing(true);
+        builder.setStyle(new NotificationCompat.MediaStyle());
+        //Pending Intent for Previous Button
+        Intent previousSongIntent = new Intent(SONG_PREVIOUS);
+        PendingIntent previousSongPendingIntent = PendingIntent.getBroadcast(this, 1, previousSongIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.addAction(new android.support.v4.app.NotificationCompat.Action(R.drawable.previous, "Previous", previousSongPendingIntent));
+        //Pending Intent for Next Button
+        Intent nextSongIntent = new Intent(SONG_NEXT);
+        PendingIntent nextSongPendingIntent = PendingIntent.getBroadcast(this, 0, nextSongIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.addAction(new android.support.v4.app.NotificationCompat.Action(R.drawable.next, "Next", nextSongPendingIntent));
+        //Start Notification
+        startForeground(FOREGROUND_NOTIFICATION, builder.build());
     }
 }
